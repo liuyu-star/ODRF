8f468fcf2dc50871c2215c338e2d6b189decf6eb
Yu liu <liuyuchina123@gmail.com>
2023-03-19 12:17:45 +0800

    Update best_cut_node.R

--- R/best_cut_node.R
+++ R/best_cut_node.R
@@ -6,7 +6,7 @@
 #' @param y A response vector of length n.
 #' @param split One of three criteria, 'gini': gini impurity index (classification), 'entropy': information gain (classification)
 #' or 'mse': mean square error (regression).
-#' @param lambda The adjustment parameter of \code{split} is used to determine whether to split or not, with the available values being 0, 1 and 'log' (Default).
+#' @param lambda The argument of \code{split} is used to determine the penalty level of the partition criterion. Three options are provided including, \code{lambda=0}: no penalty; \code{lambda=2}: AIC penalty; \code{lambda='log'} (Default): BIC penalty. In Addition, lambda can be any value from 0 to n (training set size).
 #' @param MinLeaf Minimal node size (Default 10).
 #' @param weights A vector of values which weigh the samples when considering a split.
 #' @param numLabels The number of categories.
@@ -15,7 +15,7 @@
 #' \itemize{
 #' \item BestCutVar: The best split variable.
 #' \item BestCutVal: The best split points for the best split variable.
-#' \item BestIndex: Each variable corresponds to the min gini impurity index, the max information gain or the min mean square error.
+#' \item BestIndex: Each variable corresponds to maximum decrease in gini impurity index, information gain, and mean square error.
 #' }
 #'
 #' @examples
